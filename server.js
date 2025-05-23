8ef9d64210761e4c0e7a73070aed6b18a9d36219
panchajanyabollam <b.panchajanya@gmail.com>
2025-05-23 15:44:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+import server from "./app.js";
+import { connectDB } from "./config/db.js";
+
+console.log("Starting server...");
+
+const serverStar = server.listen(process.env.PORT, async (err) => {
+  if (err) {
+    console.log(`server failed with error ${err}`);
+  } else {
+    await connectDB();
+    console.log(`server is running at http://localhost:${process.env.PORT}`);
+  }
+});
